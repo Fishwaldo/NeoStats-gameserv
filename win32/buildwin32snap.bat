92b227ee85d6cad50d783be30f5d51afdb20974e
Fish <>
2008-03-18 12:32:27 +0000

    Bump Win32 Version

--- win32/buildwin32snap.bat
+++ win32/buildwin32snap.bat
@@ -1,4 +1,4 @@
 @echo off
 IF EXIST win32\GamesServ-Setup*.exe del win32\GamesServ-Setup*.exe
 "C:\Program Files\Pantaray\QSetup\Composer.exe" win32\GamesServ.qsp /Compile /Exit
-move win32\GamesServ-Setup.exe win32\GamesServ-Setup-3-0-0-%1.exe
+move win32\GamesServ-Setup.exe win32\GamesServ-Setup-3-0-1-%1.exe
